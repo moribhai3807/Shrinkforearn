6eb58f5d39e90a4b21ce401f984503a4fa0fd44d
moribhai3807 <62470879+moribhai3807@users.noreply.github.com>
2020-12-16 20:09:46 +0530

    Add files via upload

---
+++ Bot/config.php
@@ -0,0 +1,20 @@
+<?php
+//=========BOT TOKEN==========//
+$BOT_TOKEN = $_ENV['BOT_TOKEN'];
+
+//=========BOT USERNAME======//
+$BOT_USERNAME = $_ENV['BOT_USERNAME'];
+
+//=========GPLINKS API KEY=========//
+$SE_API_KEY = $_ENV['SE_API_KEY'];
+
+//=========OTHERS=======//
+$START_MESSAGE = $_ENV["START_MESSAGE"] =<<<EOM
+
+<b>I'm a SHRINKFOREARN Shortner Bot
+
+Just Send Me A Long Url. I Will Give You It's Short Url.
+
+</b>
+EOM;
+?>
